b5f745ccdb4082f16986cb68045f6537b4397e64
Gabriela Oliveira <you@example.com>
2026-01-09 09:39:24 -0300

    feat: Coloca o Lombok ba classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Model/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Model/MissoesModel.java
@@ -2,11 +2,15 @@ package dev.java10x.CadastroDeNinjas.Missoes.Model;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.Model.NinjaModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes" )
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Model/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Model/NinjaModel.java
@@ -2,11 +2,15 @@ package dev.java10x.CadastroDeNinjas.Ninjas.Model;
 
 import dev.java10x.CadastroDeNinjas.Missoes.Model.MissoesModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -24,37 +28,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
