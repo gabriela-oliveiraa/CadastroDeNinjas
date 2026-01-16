89562f2ef3594b42b16265d23fd346a58c503a8b
Gabriela Oliveira <you@example.com>
2026-01-15 21:16:05 -0300

    Chore: Inclui a versao do DTO pela entidade e mapeia manualmente no NinjaMapper e MissoesMapper os caminhos entidade x dto

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Mapper/MissoesMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/Mapper/MissoesMapper.java
@@ -1,4 +1,32 @@
 package dev.java10x.CadastroDeNinjas.Missoes.Mapper;
 
+import dev.java10x.CadastroDeNinjas.Missoes.DTO.MissoesDTO;
+import dev.java10x.CadastroDeNinjas.Missoes.Model.MissoesModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
+
+    public MissoesModel map(MissoesDTO missoesDTO) {
+        MissoesModel missoesModel = new MissoesModel();
+
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesModel.setNinjas(missoesDTO.getNinjas());
+
+        return missoesModel;
+    }
+
+    public MissoesDTO map(MissoesModel missoesModel) {
+
+        MissoesDTO missoesDTO = new MissoesDTO();
+
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNome(missoesModel.getNome());
+        missoesDTO.setDificuldade(missoesModel.getDificuldade());
+        missoesDTO.setNinjas(missoesModel.getNinjas());
+
+        return missoesDTO;
+    }
 }
